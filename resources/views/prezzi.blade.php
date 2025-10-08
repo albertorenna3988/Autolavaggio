3563a895de933baad8015d43cbda1ce7eea2ae87
alberto_renna <albertorenna3988@gmail.com>
2025-10-08 18:00:47 +0200

    Commit

--- resources/views/prezzi.blade.php
+++ resources/views/prezzi.blade.php
@@ -2,19 +2,30 @@
 
 @section('content')
 <style>
+    .prezzi-fullscreen {
+        padding-top: 80px; /* Spazio sotto la navbar */
+    }
+
+    @media (max-width: 768px) {
+        .prezzi-fullscreen {
+            padding-top: 100px; /* Più spazio sui dispositivi mobili */
+        }
+    }
+
     .prezzi-fullscreen img {
         width: 100%;
         height: auto;
         display: block;
         margin: 0 auto;
     }
+
     .main-content {
         padding-top: 0;
     }
 </style>
 
 <div class="prezzi-fullscreen">
-    <img src="{{ asset('images/listino.jpg') }}" alt="">
-    <img src="{{ asset('images/listino2.jpg') }}" alt="">
+    <img src="{{ asset('images/listino.jpg') }}" alt="Listino prezzi 1">
+    <img src="{{ asset('images/listino2.jpg') }}" alt="Listino prezzi 2">
 </div>
 @endsection
